cd1a9261c699f8730b0cd0b6ad61e68421b59243
Ben Best <bdbest@gmail.com>
2013-12-19 12:11:55 -0800

    consolidated install script

--- inst/install_ohi/install_code.R
+++ inst/install_ohi/install_code.R
@@ -3,6 +3,8 @@ if (!require(devtools)){
   require(devtools)
 } 
 
+# for production version, use github.user = 'ohi-science'
+# for development version, use github.user = 'bbest'
 github.user = 'ohi-science'
 install_github('rCharts', github.user)
 install_github('ohicore', github.user)
